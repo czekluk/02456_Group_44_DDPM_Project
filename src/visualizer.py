6117640d56d37c3fcc6fbb9cda27c9cb8143ba66
czekluk <lukas_czekalla@gmx.de>
2024-11-22 12:31:28 +0100

    made visualizer cifar10 ready

--- src/visualizer.py
+++ src/visualizer.py
@@ -97,21 +97,29 @@ class Visualizer:
         plt.savefig(os.path.join(save_path, fig_name))
         plt.close()
 
+    def denormalize(self, image: np.ndarray):
+        '''
+        Denormalize image from [-1, 1] to [0, 1]
+        '''
+        return (image + 1) / 2
+
     def plot_single_image(self, image: np.ndarray, 
                           save_path: str = os.path.join(PROJECT_BASE_DIR,'results','images','generated'),
                           fig_name: str = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}-Generated-Image.png",
-                          title: str = 'Generated Image', cmap: str = 'gray'):
+                          title: str = 'Generated Image', cmap: str = 'gray', denormalize: bool = True):
         '''
         Plot a single image
 
         Inputs:
-        - image: Image to plot
+        - image: Image to plot in shape [B, C, H, W]
         - save_path: Path to save-directory
         - fig_name: Name of the saved file
         '''
         # create plot
         fig, ax = plt.subplots(figsize=(10,6))
-        #image = (image + 1)/2
+        if denormalize:
+            image = self.denormalize(image)
+        image = np.transpose(image[0],(1,2,0))
         ax.imshow(image, cmap=cmap)
         ax.set_title(title)
         ax.axis('off')
@@ -125,24 +133,27 @@ class Visualizer:
     def plot_reconstructed_image(self, original_image: np.ndarray, reconstructed_image: np.ndarray,
                                  save_path: str = os.path.join(PROJECT_BASE_DIR,'results','images','reconstructed'),
                                  fig_name: str = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}-Reconstructed-Image.png",
-                                 title: str = 'Reconstructed Image', cmap: str = 'gray'):
+                                 title: str = 'Reconstructed Image', cmap: str = 'gray', denormalize: bool = True):
         '''
         Plot two images side by side.
         Original image on the left and reconstructed image on the right.
 
         Inputs:
-        - original_image: Original image
+        - original_image: Original image in shape [B, C, H, W]
         - reconstructed_image: Reconstructed image
         - save_path: Path to save-directory
         - fig_name: Name of the saved file
         '''
+        if denormalize:
+            original_image = self.denormalize(original_image)
+            reconstructed_image = self.denormalize(reconstructed_image)
+        original_image = np.transpose(original_image[0],(1,2,0))
+        reconstructed_image = np.transpose(reconstructed_image[0],(1,2,0))
         # create plot
         fig, ax = plt.subplots(1, 2, figsize=(10,6))
-        #original_image = (original_image + 1) / 2
         ax[0].imshow(original_image, cmap=cmap)
         ax[0].set_title('Original Image')
         ax[0].axis('off')
-        #reconstructed_image = (reconstructed_image + 1) / 2
         ax[1].imshow(reconstructed_image, cmap=cmap)
         ax[1].set_title('Reconstructed Image')
         ax[1].axis('off')
@@ -157,7 +168,7 @@ class Visualizer:
     def plot_multiple_images(self, images: np.ndarray, 
                              save_path: str = os.path.join(PROJECT_BASE_DIR,'results','images','generated'),
                              fig_name: str = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}-Generated-Images.png",
-                             title: str = 'Generated Images', cmap: str = 'gray'):
+                             title: str = 'Generated Images', cmap: str = 'gray', denormalize: bool = True):
         '''
         Plot multiple images in a grid
 
@@ -172,6 +183,9 @@ class Visualizer:
             print('Only the first 16 images will be plotted')
         num_rows = int(np.ceil(images.shape[0] / 4))
 
+        if denormalize:
+            images = self.denormalize(images)
+
         # create plot
         fig, ax = plt.subplots(num_rows, 4, figsize=(10,10))
         for i in range(num_rows):
@@ -179,13 +193,11 @@ class Visualizer:
                 if num_rows > 1:
                     if i*4+j < images.shape[0]:
                         img = np.transpose(images[i*4+j],(1,2,0))
-                        #img = (img + 1) / 2
                         ax[i,j].imshow(img, cmap=cmap)
                     ax[i,j].axis('off')
                 else:
                     if j < images.shape[0]:
                         img = np.transpose(images[j],(1,2,0))
-                        #img = (img + 1) / 2
                         ax[j].imshow(img, cmap=cmap)
                     ax[j].axis('off')
         fig.suptitle(title)
@@ -199,7 +211,7 @@ class Visualizer:
     def plot_reconstructed_images(self, original_images: List[np.ndarray], reconstructed_images: List[np.ndarray],
                                   save_path: str = os.path.join(PROJECT_BASE_DIR,'results','images','reconstructed'),
                                   fig_name: str = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}-Reconstructed-Images.png",
-                                  title: str = 'Reconstructed Images', cmap: str = 'gray'):
+                                  title: str = 'Reconstructed Images', cmap: str = 'gray', denormalize: bool = True):
         '''
         Plot multiple pairs of images side by side.
         Original images on the left and reconstructed images on the right.
@@ -217,6 +229,10 @@ class Visualizer:
             reconstructed_images = reconstructed_images[:16]
             print('Only the first 16 images will be plotted')
 
+        if denormalize:
+            original_images = self.denormalize(original_images)
+            reconstructed_images = self.denormalize(reconstructed_images)
+
         num_rows = int(np.ceil(original_images.shape[0] / 4))
         # create plot
         fig, ax = plt.subplots(num_rows, 4*2, figsize=(10,10))
@@ -226,24 +242,20 @@ class Visualizer:
                     if num_rows > 1:
                         if j % 2 == 0:
                             img = np.transpose(original_images[i*4+(j//2)], (1,2,0))
-                            #img = (img + 1) / 2
                             ax[i,j].imshow(img, cmap=cmap)
                             ax[i,j].set_title('Orig.')
                         else:
                             img = np.transpose(reconstructed_images[i*4+(j//2)],(1,2,0))
-                            #img = (img + 1) / 2
                             ax[i,j].imshow(img, cmap=cmap)
                             ax[i,j].set_title('Reconst.')
                         ax[i,j].axis('off')
                     else:
                         if j % 2 == 0:
                             img = np.transpose(original_images[j//2],(1,2,0))
-                            #img = (img + 1) / 2
                             ax[j].imshow(img, cmap=cmap)
                             ax[j].set_title('Orig.')
                         else:
                             img = np.transpose(reconstructed_images[j//2],(1,2,0))
-                            #img = (img + 1) / 2
                             ax[j].imshow(img, cmap=cmap)
                             ax[j].set_title('Reconst.')
                         ax[j].axis('off')
@@ -258,7 +270,7 @@ class Visualizer:
     def plot_forward_process(self, diffusion_model: DiffusionModel, x: torch.Tensor, t: List[int], 
                              save_path: str = os.path.join(PROJECT_BASE_DIR,'results','images','forward_process'),
                              fig_name: str = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}-Forward-Process.png",
-                             title: str = 'Forward Process', cmap: str = 'gray'):
+                             title: str = 'Forward Process', cmap: str = 'gray', denormalize: bool = True):
         '''
         Plot the forward process of the diffusion model
 
@@ -273,9 +285,9 @@ class Visualizer:
         for idx in range(len(t)):
             x_t = diffusion_model.forward(x, t[idx])
             x_t = x_t.permute(0, 2, 3, 1).detach().cpu().numpy()
-            #x_t = (x_t[0] + 1) / 2
-            x_t = x_t[0]
-            ax[idx].imshow(x_t, cmap=cmap)
+            if denormalize:
+                x_t = self.denormalize(x_t)
+            ax[idx].imshow(x_t[0], cmap=cmap)
             ax[idx].axis('off')
             ax[idx].set_title(f't={t[idx]}')
         fig.suptitle(title)
@@ -289,7 +301,7 @@ class Visualizer:
     def plot_reverse_process(self, samples: List[np.ndarray], t: List[int], 
                              save_path: str = os.path.join(PROJECT_BASE_DIR,'results','images','reverse_process'),
                              fig_name: str = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}-Reverse-Process.png",
-                             title: str = 'Reverse Process', cmap: str = 'gray'):
+                             title: str = 'Reverse Process', cmap: str = 'gray', denormalize: bool = True):
         '''
         Plot the reverse process of the diffusion model
 
@@ -303,7 +315,8 @@ class Visualizer:
         fig, ax = plt.subplots(1, len(t), figsize=(10,6))
         for idx in range(len(t)):
             sample = np.transpose(samples[t[idx]][0],(1,2,0))
-            #sample = (sample + 1) / 2
+            if denormalize:
+                sample = self.denormalize(sample)
             ax[idx].imshow(sample, cmap=cmap)
             ax[idx].axis('off')
             ax[idx].set_title(f't={t[idx]}')
